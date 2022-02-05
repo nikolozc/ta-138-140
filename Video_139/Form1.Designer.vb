37b12156578e5abbcb02df7c3f2d97dc7a0b4246
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-05 18:07:49 -0500

    finished 139 and 140
    finished 139 and 140

---
+++ Video_139/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnRecursion = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(21, 31)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(224, 23)
+        Me.TextBox1.TabIndex = 0
+        '
+        'btnRecursion
+        '
+        Me.btnRecursion.Location = New System.Drawing.Point(251, 30)
+        Me.btnRecursion.Name = "btnRecursion"
+        Me.btnRecursion.Size = New System.Drawing.Size(75, 23)
+        Me.btnRecursion.TabIndex = 1
+        Me.btnRecursion.Text = "Recursion"
+        Me.btnRecursion.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(338, 86)
+        Me.Controls.Add(Me.btnRecursion)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnRecursion As Button
+End Class
---
+++ Video_139/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub btnRecursion_Click(sender As Object, e As EventArgs) Handles btnRecursion.Click
+        MessageBox.Show(RecursiveFunction(Convert.ToInt32(TextBox1.Text)))
+    End Sub
+
+    Private Function RecursiveFunction(ByVal argInt As Integer) As Double
+        If argInt <= 10 Then
+            Return argInt
+        End If
+        If argInt > 10 Then
+            Return RecursiveFunction(argInt / 2)
+        End If
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text.Length > 0 Then
+            Try
+                Convert.ToDouble(TextBox1.Text)
+            Catch ex As Exception
+                MessageBox.Show("please only type numbers")
+                TextBox1.Clear()
+            End Try
+        End If
+    End Sub
+End Class
